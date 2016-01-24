c3eedba0082553e818cd0a84354e6906a22f4187
Salman Hashmi <shash24@hotmail.ca>
2016-01-24 18:46:24 -0500

    format earthquake-key elements

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -307,56 +307,65 @@ public class EarthquakeCityMap extends PApplet {
 		
 		rect(xbase, ybase, 500, 1300);
 		
+		
 		fill(0);
 		textAlign(LEFT, CENTER);
-		textSize(12);
-		text("Earthquake Key", xbase+25, ybase+25);
+		textSize(27);
+		text("Earthquake Key", xbase+125, ybase+25);
+		
 		
 		fill(150, 30, 30);
-		int tri_xbase = xbase + 35;
-		int tri_ybase = ybase + 50;
+		int tri_xbase = xbase + 104;
+		int tri_ybase = ybase + 80;
 		triangle(tri_xbase, tri_ybase-CityMarker.TRI_SIZE, tri_xbase-CityMarker.TRI_SIZE, 
 				tri_ybase+CityMarker.TRI_SIZE, tri_xbase+CityMarker.TRI_SIZE, 
 				tri_ybase+CityMarker.TRI_SIZE);
 
-		fill(0, 0, 0);
 		textAlign(LEFT, CENTER);
-		text("City Marker", tri_xbase + 15, tri_ybase);
+		fill(0, 0, 0);
+		text("City Marker", tri_xbase+60, tri_ybase);
 		
-		text("Land Quake", xbase+50, ybase+70);
-		text("Ocean Quake", xbase+50, ybase+90);
-		text("Size ~ Magnitude", xbase+25, ybase+110);
-		
+		fill(0, 0, 0);
+		text("Land Quake", xbase+160, ybase+120);
 		fill(255, 255, 255);
-		ellipse(xbase+35, 
-				ybase+70, 
-				10, 
-				10);
-		rect(xbase+35-5, ybase+90-5, 10, 10);
+		ellipse(xbase+105, ybase+120, 20, 20);
 		
+		fill(0, 0, 0);
+		text("Ocean Quake", xbase+160, ybase+160);
+		fill(255, 255, 255);
+		rect(xbase+105-10, ybase+160-10, 20, 20);
+		
+		
+		fill(0, 0, 0);
+		text("Size ~ Magnitude", xbase+100, ybase+220);
+		
+		
+		textAlign(LEFT, CENTER);
+		fill(0, 0, 0);
+		text("Shallow", xbase+150, ybase+280);
 		fill(color(255, 255, 0));
-		ellipse(xbase+35, ybase+140, 12, 12);
-		fill(color(0, 0, 255));
-		ellipse(xbase+35, ybase+160, 12, 12);
-		fill(color(255, 0, 0));
-		ellipse(xbase+35, ybase+180, 12, 12);
+		ellipse(xbase+105, ybase+280, 22, 22);
 		
-		textAlign(LEFT, CENTER);
 		fill(0, 0, 0);
-		text("Shallow", xbase+50, ybase+140);
-		text("Intermediate", xbase+50, ybase+160);
-		text("Deep", xbase+50, ybase+180);
-
-		text("Past hour", xbase+50, ybase+200);
+		text("Intermediate", xbase+150, ybase+320);
+		fill(color(0, 0, 255));
+		ellipse(xbase+105, ybase+320, 22, 22);
 		
+		fill(0, 0, 0);
+		text("Deep", xbase+150, ybase+360);
+		fill(color(255, 0, 0));
+		ellipse(xbase+105, ybase+360, 22, 22);
+
+		fill(0, 0, 0);
+		text("Past hour", xbase+150, ybase+400);
 		fill(255, 255, 255);
-		int centerx = xbase+35;
-		int centery = ybase+200;
-		ellipse(centerx, centery, 12, 12);
+		int centerx = xbase+105;
+		int centery = ybase+400;
+		ellipse(centerx, centery, 22, 22);
 
 		strokeWeight(2);
-		line(centerx-8, centery-8, centerx+8, centery+8);
-		line(centerx-8, centery+8, centerx+8, centery-8);
+		line(centerx-12, centery-12, centerx+12, centery+12);
+		line(centerx-12, centery+12, centerx+12, centery-12);
 			
 	}
 
